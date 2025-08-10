3b3536876aa4065835dde31354a4438630adcb13
Joseph Tadesse <josephtadessem@gmail.com>
2025-08-10 16:13:37 +0300

    Add main file to scan the urls

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func checkURL(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	client := http.Client{Timeout: 2 * time.Second}
+	resp, err := client.Get(url)
+
+	if err != nil {
+		fmt.Printf("❌ %s (error: %v)\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		fmt.Printf("✅ %s (status: %d)\n", url, resp.StatusCode)
+	} else {
+		fmt.Printf("❌ %s (status: %d)\n", url, resp.StatusCode)
+
+	}
+
+}
+
+func main() {
+	data, err := os.Open("urls.txt")
+	check(err)
+	defer data.Close()
+
+	var wg sync.WaitGroup
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		url := scanner.Text()
+		wg.Add(1)
+		go checkURL(url, &wg)
+	}
+	wg.Wait()
+
+}
